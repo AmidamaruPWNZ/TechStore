ac1782b7d31ed35196aac603b9efc9daf8ada7d2
AmidamaruPWNZ <nikita14915@gmail.com>
2024-03-22 16:49:38 +0500

    Just commited

---
+++ Models/Repository/EFDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Data.Entity;
+
+namespace GameStore.Models.Repository
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext()
+            : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\nikit\Desktop\TechStore\App_Data\TechStore.mdf;Integrated Security=True"
+                    + ";Integrated Security=True")
+        {  }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
